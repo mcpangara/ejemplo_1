5caf369b3b0f339bb03d4065d38e0b637bfac9ab
William Angarita <mcpangara@gmail.com>
2025-03-09 21:52:11 -0500

    RUTA PAR ACONSULTA DE DATOS EN EL BACKEND

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+let PATH_DATOS = "https://jsonplaceholder.typicode.com/todos";
+
 createApp(App).mount('#app');
